649b89abdf35e7e678d4ebed7a5497b5f6a3a978
胡铮铮 <ex-huzhengzheng@jinbaochuang.com>
2019-07-26 10:03:04 +0800

    修改配置

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 module.exports = {
+  publicPath: './',
   //设置为 true 时，eslint-loader 会将 lint 错误输出为编译警告。默认情况下，警告仅仅会被输出到命令行，且不会使得编译失败。
   lintOnSave: false,
   //webpack配置
